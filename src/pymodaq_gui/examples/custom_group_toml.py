53ed3706e3980d2100b30c7ff8a1a7601eb431a0
Loic Guilmard <loic.guilmard@cnrs.fr>
2024-12-06 15:46:36 +0100

    added a CustomQDialog

--- src/pymodaq_gui/examples/custom_group_toml.py
+++ src/pymodaq_gui/examples/custom_group_toml.py
@@ -7,6 +7,11 @@ from pymodaq_utils import config as config_mod
 from pymodaq_gui.utils.widgets.tree_toml import TreeFromToml
 from pymodaq_gui.parameter.pymodaq_ptypes import registerParameterType, GroupParameter
 
+from pyqt_checkbox_list_widget.checkBoxListWidget import CheckBoxListWidget
+# from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QCheckBox
+from qtpy.QtWidgets import QDialog, QVBoxLayout, QPushButton, QCheckBox
+
+
 config_path = Path('./custom_toml_group.toml')
 
 
@@ -21,6 +26,57 @@ child_template = [
     {'title': 'Choose:', 'name': 'choice', 'type': 'list', 'value': 'ok',
      'limits': ['ok', 'nonok']},
 ]
+
+
+channel_list = [f"Instrument1/Module2/Channel{number}" for number in range(10)]
+
+
+class CustomDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        # The answer we've all been looking for
+        self.channellist = []
+
+        self.setWindowTitle("Select Channels")
+        self.setLayout(QVBoxLayout())
+
+        self.allCheckBox = QCheckBox('Check all')
+        self.cblist = CheckBoxListWidget()
+        self.cblist.addItems(channel_list)
+        self.layout().addWidget(self.allCheckBox)
+        self.layout().addWidget(self.cblist)
+
+        # label = QLabel("Enter your name:")
+        # self.layout().addWidget(label)
+
+        # self.nameEdit = QLineEdit()
+        # self.layout().addWidget(self.nameEdit)
+
+        button = QPushButton("Submit")
+        button.clicked.connect(self.submit_name)
+        self.layout().addWidget(button)
+
+    def submit_name(self):
+        self.channellist = self.checkedlist(self.cblist)
+        self.accept()
+
+    def checkedlist(self, widget: CheckBoxListWidget):
+        """
+        Return the value select from a Checkbox list
+        Parameters
+        ----------
+        widget that is a CheckBoxListWidget
+
+        Returns
+        -------
+        list of value
+        """
+        if isinstance(widget, CheckBoxListWidget):
+            return [item.text()
+                    for item in [widget.item(counter) for counter in range(widget.count())]
+                    if item.checkState() == 2]
+        else:
+            return self.checkedlist(self.cblist)
 
 
 class ScalableCustomGroup(GroupParameter):
